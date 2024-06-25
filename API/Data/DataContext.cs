a7888092a53a3429372bf2d5961c2795190bd554
hunglq <104688414+1001VND@users.noreply.github.com>
2024-06-26 01:06:17 +0700

    adding the like user feature

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -4,12 +4,33 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data
 {
-	public class DataContext : DbContext
-	{
-		public DataContext(DbContextOptions options) : base(options)
-		{
-		}
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
 
-		public DbSet<AppUser> Users { get; set; }
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.TargetUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }

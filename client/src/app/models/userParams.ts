3f25bc705ae9868ed8aab94a57c8de54c857c3f4
hunglq <104688414+1001VND@users.noreply.github.com>
2024-07-07 17:34:43 +0700

    change user param

--- client/src/app/models/userParams.ts
+++ client/src/app/models/userParams.ts
@@ -3,7 +3,7 @@ import { User } from "./user";
 export class UserParams {
     gender: string;
     minAge = 18;
-    maxAge = 30;
+    maxAge = 99;
     pageNumber = 1;
     pageSize = 6;
     orderBy = 'lastActive';
